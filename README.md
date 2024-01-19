f9e55f60e2144b2d77ffa77cd25c774705312f16
lalauo <luwa.oye@gmail.com>
2024-01-19 16:38:01 +0000

    moved listen.js to root folder

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Northcoders News API
+# Northcoders News API - PSQL database and node-postgres interface
+
 
 Create environment variables to run locally: 
 - Add .env test and development files to connect to databases
--- db/connection.js
+++ db/connection.js
@@ -5,15 +5,16 @@ const pathToCorrectEnvFile = `${__dirname}/../.env.${ENV}`;
 
 const config = {};
 
+require("dotenv").config({
+  path: pathToCorrectEnvFile,
+});
+
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
 
-require("dotenv").config({
-  path: pathToCorrectEnvFile,
-});
-
+console.log(pathToCorrectEnvFile);
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
